f9c9decac56262383ce50538d2bd6db5eee3501a
Marcin Czekajski <czekajskimarcin@gmail.com>
2021-06-12 12:08:20 +0200

    ref example

--- src/components/MyAwesomeComponent.vue
+++ src/components/MyAwesomeComponent.vue
@@ -1,9 +1,25 @@
 <template>
-  <div>My Awesome component</div>
+  <div>
+    <button @click="incrementPension">Increment my pension</button>
+    <p>Your pension will be: {{ pension }}</p>
+  </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
-    name: "MyAwesomeComponent"
+  name: "MyAwesomeComponent",
+  setup() {
+    const pension = ref(20);
+
+    function incrementPension() {
+      pension.value++;
+    }
+
+    return {
+      pension, incrementPension
+    };
+  },
 };
 </script>
